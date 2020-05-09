17798db2f06fbd3a87aa8ff20cc3eea120547c57
Chris <chris@tracktownsoftware.com>
2020-05-09 11:14:45 -0700

    Clean up solution to target WPF .Net Framework custom control

--- Pack/packAll.bat
+++ Pack/packAll.bat
@@ -1,6 +1,4 @@
 set PACKAGEVERSION=1.2.44
-call packWinformNetCore >packAll.new
-call packWinformNetFramework >>packAll.new
-call packWpfNetCore >>packAll.new
-call packWpfNetFramework >>packAll.new
-echo Done >>packAll.new
+msbuild ..\WPFControlNetFramework\WPFControlNetFramework.csproj -p:Configuration=Release 
+msbuild ..\WPFControlNetFramework.Design\WPFControlNetFramework.Design.csproj -p:Configuration=Release 
+..\libs\nuget.exe pack ..\WpfControlNetFramework\WpfControlNetFramework.nuspec -Version %PACKAGEVERSION% -OutputDirectory Packages
